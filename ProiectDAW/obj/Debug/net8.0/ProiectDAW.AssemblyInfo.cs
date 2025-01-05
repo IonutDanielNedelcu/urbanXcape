45a602289a64f9d58d30fb47eb64b8afaec08476
DanielNedelcu <ionut-daniel.nedelcu@s.unibuc.ro>
2025-01-05 13:18:12 +0200

    Rezolvare root logout

--- ProiectDAW/obj/Debug/net8.0/ProiectDAW.AssemblyInfo.cs
+++ ProiectDAW/obj/Debug/net8.0/ProiectDAW.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProiectDAW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644043adf9209f9021adb2aaafc0958bc92fda5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4629d5f27051e7d49db9faf63fb47d1a4ecbacb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProiectDAW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProiectDAW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
